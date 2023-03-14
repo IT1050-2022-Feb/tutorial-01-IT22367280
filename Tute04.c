dbb05c19c71aad694e156ea29e3895aa92ed2db3
IT22367280 <121474789+IT22367280@users.noreply.github.com>
2023-03-14 21:13:21 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -7,6 +7,25 @@ Do not change the code given in the main() function when you are implementing yo
 #include <stdio.h>
 
 int main() {
+   if (no1<no2)
+    return no1;
+  else 
+  {
+    return no2;
+  }
+}
+int maximum(no1,no2)
+{
+  if (no1>no2)
+    return no1;
+  else
+  {
+    return no2;
+  }
+}
+int multipliy(no1,no2)
+{ return no1*no2;
+  }
    int no1, no2;
    printf("Enter a value for no 1 : ");
    scanf("%d", &no1);
